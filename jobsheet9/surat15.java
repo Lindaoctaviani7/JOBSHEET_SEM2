0e403d2713e2fe4e531a8d44ee3b223275cc4945
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2025-04-29 12:41:28 +0700

    a

---
+++ jobsheet9/surat15.java
@@ -0,0 +1,15 @@
+public class surat15 {
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    public surat15(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+}
