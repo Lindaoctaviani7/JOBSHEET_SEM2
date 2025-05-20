0449b5ceadface0921d4386bc0b337230ec36182
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2025-05-20 13:31:57 +0700

    Add files via upload

---
+++ Node15.java
@@ -0,0 +1,10 @@
+public class Node15 {
+    int data;
+    Node15 prev, next;
+
+    public Node15(Node15 prev, int data, Node15 next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
